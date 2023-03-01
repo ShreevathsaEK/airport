c4374733d9d7043faafe12197d7422cadc57860f
ShreevathsaEK <126555689+ShreevathsaEK@users.noreply.github.com>
2023-03-01 16:41:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+import  ReactDOM  from "react-dom";
+import HomePage from "./HomePage.js";
+ReactDOM.render(
+ <>
+ <HomePage/>
+ </>,
+  document.getElementById("root")
+)
