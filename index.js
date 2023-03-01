5ab2db684e8fa409e806143eadcd2ff4a0a38848
ShreevathsaEK <126555689+ShreevathsaEK@users.noreply.github.com>
2023-03-01 16:48:25 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"; 
-import  ReactDOM  from "react-dom";
-import HomePage from "./HomePage.js";
-ReactDOM.render(
- <>
- <HomePage/>
- </>,
-  document.getElementById("root")
-)
