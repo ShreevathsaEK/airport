e4550a8e7c43f1e31aff25701f4ef3b3d6eb4895
ShreevathsaEK <126555689+ShreevathsaEK@users.noreply.github.com>
2023-03-04 14:28:22 +0530

    Update SearchForm.js

--- src/pages/SearchForm.js
+++ src/pages/SearchForm.js
@@ -1,25 +1,38 @@
 import { useState } from "react";
 const SearchForm = () =>{
-        const [departureAirport,setDepartureAirport] = useState('');
+        const [departureAirport,setDepartureAirport] = useState("");
         const [checkin,setCheckin] = useState('');
         const [checkout,setCheckout] = useState('');
-    
+        const [errors,seterrors] =useState( {
+            departureAirport:false,
+            checkin:false,
+            checkout:false
+        })
     
         const DepartureAirportHandler = (e) => {
             //event.target.value
             const {value} = e.target;    
                 setDepartureAirport(value);
+                if(e.target){
+                    seterrors({...errors,departureAirport:false})
+                }
             }
         const checkinHandler = (e) => {
             //event.target.value
             const {value} = e.target;
             setCheckin(value);
+            if(e.target){
+                seterrors({...errors,checkin:false})
+            }
     
         }
         const checkoutHandler = (e) => {
             //event.target.value
             const {value} = e.target;
             setCheckout(value);
+            if(e.target){
+                seterrors({...errors,checkout:false})
+            }
     
         }
     
@@ -29,6 +42,15 @@ const SearchForm = () =>{
             console.log(checkout)
             //event.target.value
             e.preventDefault();
+            if (departureAirport && checkin && checkout){
+                alert("Form has been submitted");
+            }else{
+                seterrors({
+                    departureAirport:!departureAirport,
+                    checkin:!checkin,
+                    checkout:!checkout
+                });
+            }
             
         }
     return(
@@ -41,6 +63,7 @@ const SearchForm = () =>{
                                                     value={departureAirport}  
                                                     placeholder="Departure Airport" 
                                                     className="placeholder placeholder-airport" />
+                                                    {(errors.departureAirport?<div><br/><div  style={{border:1,backgroundColor:"#F28585"}}><h5><em>Invalid Departure Airport</em></h5></div></div>:null)}
                                                 </div> <i
                                                     className="fas fa-map-marker-alt input-icon"></i>
                                             </label>
@@ -49,11 +72,12 @@ const SearchForm = () =>{
                                                     <div className="heading mb-1">Parking Check-In</div>
                                                     <div className="placeholder">
                                                         <input name="checkin" type="date" onChange={checkinHandler} placeholder="Parking Check-Out" className="placeholder placeholder-airport" style={{width:'100%'}}/>
+                                                        {(errors.checkin?<div><br/><div  style={{border:1,backgroundColor:"#F28585"}}><h5><em>Invalid check-in date</em></h5></div></div>:null)}
                                                     </div> 
                                                 </label> <label className="col-sm-6 p-0 pl-sm-0 date_input">
                                                     <div className="heading mb-1">Parking Check-Out</div>
                                                         <input name="Check-Out" type="date" onChange={checkoutHandler} placeholder="Parking Check-Out" className="placeholder placeholder-airport" style={{width:"100%"}}/>
-                                                   
+                                                        {(errors.checkout?<div><br/><div  style={{border:1,backgroundColor:"#F28585"}}><h5><em>Invalid check-out date</em></h5></div></div>:null)}
                                                 </label></div>
                                             <div className="col-12 col-xl-2 p-0 pl-xl-3 my-3 my-xl-0">
                                                 <div className="d-none d-xl-block heading mb-1 invisible">Submit</div>
