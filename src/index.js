1b0e2340d1b67e67c549e08203d0751d18d696c8
ShreevathsaEK <126555689+ShreevathsaEK@users.noreply.github.com>
2023-03-09 15:26:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react"; 
 import  ReactDOM  from "react-dom";
 import HomePage from "./HomePage.js";
+import App from './App';
 ReactDOM.render(
  <>
- <HomePage/>
+ <App/>
  </>,
   document.getElementById("root")
 )
