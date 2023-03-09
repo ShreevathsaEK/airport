51c8566d1cb3e698804f94e1967b7be117273224
ShreevathsaEK <126555689+ShreevathsaEK@users.noreply.github.com>
2023-03-09 15:25:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
-ww
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./components/Homepage";
+import Layout from "./components/Loyout";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" index element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Homepage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
