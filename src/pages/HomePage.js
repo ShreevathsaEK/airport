45dd122faa03aa1671ea9e7da33864896f9f94e6
ShreevathsaEK <126555689+ShreevathsaEK@users.noreply.github.com>
2023-03-15 23:31:53 +0530

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+import SearchForm from "./SearchForm";
+function HomePage() {
+  return (
+    <>
+      <div id="app" className="generic">
+        <div className="content">
+          <section id="home_page">
+            <div className="years-of-service">
+              <div className="container">
+                For 20 years, we've helped travelers on their way. With free
+                cancellations & a customer service team in the US, we are
+                committed to serving you.
+              </div>
+            </div>
+            <SearchForm />
+            <section id="benefits">
+              <div className="container">
+                <h5>What Can You Save with AirportParkingReservations.com?</h5>
+
+                <ul className="row">
+                  <li className="col-12 col-lg-4 p-3">
+                    <img
+                      src="/assets/check.png"
+                      alt="Tick"
+                      width="50"
+                      height="50"
+                    />
+                    <div>
+                      <h6>Save Money</h6>
+                      <p>
+                        Save up to 70% off on our site compared to the cost of
+                        on-airport parking.
+                      </p>
+                    </div>
+                  </li>
+                  <li className="col-12 col-lg-4 p-3">
+                    <img
+                      src="/assets/check.png"
+                      alt="Tick"
+                      width="50"
+                      height="50"
+                    />
+                    <div>
+                      <h6>Save Time</h6>
+                      <p>
+                        It's easy to compare parking at all major airports.
+                        <br />
+                        Booking a reservation is quick & simple!
+                      </p>
+                    </div>
+                  </li>
+                  <li className="col-12 col-lg-4 p-3">
+                    <img
+                      src="/assets/check.png"
+                      alt="Tick"
+                      width="50"
+                      height="50"
+                    />
+                    <div>
+                      <h6>Save Stress</h6>
+                      <p>
+                        Guarantee your parking spot by booking in advance. Can't
+                        make it? Cancellations are free.
+                      </p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
